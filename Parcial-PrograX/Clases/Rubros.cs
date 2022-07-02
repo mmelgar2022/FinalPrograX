7928dfd2f4f570ad19fb7982d00d4c7f989fae97
MynorVásquez <mmynnorev@gmail.com>
2022-07-02 09:50:09 -0600

    Agregar archivos de proyecto.

---
+++ Parcial-PrograX/Clases/Rubros.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial_PrograX.Clases
+{
+    public class Rubros
+    {
+        public int idRubros { get; set; }
+        public string tipo { get; set; }
+        public float porcentaje { get; set; }
+    }
+}
---
+++ Parcial-PrograX/Interfaces/Ioperacion.cs
@@ -0,0 +1,28 @@
+﻿using Parcial_PrograX.Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial_PrograX.Interfaces
+{
+    interface  Ioperacion
+    {
+        interface IAhorro
+        {
+            double calAhorro(Clientes cliente);
+        }
+        interface ICalcISR
+        {
+            double calcularISR(Clientes cliente);
+        }
+
+        interface ICalcIVA
+        {
+            double calcIVA(Clientes cliente);
+        }
+
+
+    }
+}
